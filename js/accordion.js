121c8069acdf604953ffe771247a8824ed31723d
Ali <ali@Alis-MacBook.local>
2020-02-25 03:52:21 +0100

    Added behaviour for no main title and no description panels

--- js/accordion.js
+++ js/accordion.js
@@ -2,60 +2,81 @@ function Accordion(options) {
   //Selecting the main container
   var accordionDiv = document.querySelector(`#${options.container}`);
 
-  // Create the title ,the inside paragraph element and the divider
-  let titlePanel = document.createElement("div");
-  let titleParagraph = document.createElement("p");
-  let divider = document.createElement("hr");
-  //Setting the title
-  titleParagraph.innerHTML = options.mainTitle;
+  // If the accordion has a Main Title
+  if (options.mainTitle) {
+    // Create the title ,the inside paragraph element and the divider
+    let titlePanel = document.createElement("div");
+    let titleParagraph = document.createElement("p");
+    let divider = document.createElement("hr");
 
-  //Adding classes to the div
-  titlePanel.classList.add("panel-div", "panel-title-div");
+    //Setting the title
+    titleParagraph.innerHTML = options.mainTitle;
 
-  // Appending the created elements to the root div
-  titlePanel.append(titleParagraph);
-  accordionDiv.append(titlePanel);
-  accordionDiv.append(divider);
+    //Adding classes to the div
+    titlePanel.classList.add("panel-div", "panel-title-div");
+
+    // Appending the created elements to the root div
+    titlePanel.append(titleParagraph);
+    accordionDiv.append(titlePanel);
+    accordionDiv.append(divider);
+  }
 
   // Foreach panel we create the various elements
   options.panels.forEach((panel, index) => {
     // Creating the div container of the panel
     let panelDiv = document.createElement("div");
 
+    //Creating a bool for detecting if has a description
+    let hasDescription = panel.subtitle;
+
     // Creating the title, description,button and content div with relative paragraph elements of the panel and the divider
     let panelTitleDiv = document.createElement("div");
-    let panelDescriptionDiv = document.createElement("div");
+    let panelDescriptionDiv = hasDescription
+      ? document.createElement("div")
+      : null;
     let panelContentDiv = document.createElement("div");
     let panelButtonDiv = document.createElement("div");
     let panelTitleParagraph = document.createElement("p");
-    let panelDescriptionParagraph = document.createElement("p");
+    let panelDescriptionParagraph = hasDescription
+      ? document.createElement("p")
+      : null;
     let divider = document.createElement("hr");
 
     //Setting the title,description material icon and content
     panelTitleParagraph.innerHTML = panel.title;
-    panelDescriptionParagraph.innerHTML = panel.subtitle;
+    hasDescription
+      ? (panelDescriptionParagraph.innerHTML = panel.subtitle)
+      : null;
     panelContentDiv.innerHTML += panel.content;
     panelButtonDiv.innerHTML +=
       "<i class='material-icons'>keyboard_arrow_down</i >";
 
     // Adding the classes to the panel div, title, description and content
-    panelDiv.classList.add("panel-div", "panel-with-description");
+    panelDiv.classList.add(
+      "panel-div",
+      `${hasDescription ? "panel-with-description" : "panel-no-description"}`
+    );
     panelTitleDiv.classList.add("panel-title");
     panelButtonDiv.classList.add("panel-button-div");
-    panelDescriptionDiv.classList.add("panel-description");
+    !hasDescription ? (panelButtonDiv.style.marginTop = "1%") : null;
+    hasDescription
+      ? panelDescriptionDiv.classList.add("panel-description")
+      : null;
     panelContentDiv.classList.add("panel-content");
 
     //Appending the title, description,button content to the relative div and then to the panel div
     panelTitleDiv.append(panelTitleParagraph);
-    panelDescriptionDiv.append(panelDescriptionParagraph);
+    hasDescription
+      ? panelDescriptionDiv.append(panelDescriptionParagraph)
+      : null;
     panelDiv.append(panelButtonDiv);
     panelDiv.append(panelTitleDiv);
-
-    panelDiv.append(panelDescriptionDiv);
+    hasDescription ? panelDiv.append(panelDescriptionDiv) : null;
     panelDiv.append(panelContentDiv);
 
     // Appending the panel div to the root div
     accordionDiv.append(panelDiv);
+
     // Appending the divider to all the iterations beside the last one
     index !== options.panels.length - 1 ? accordionDiv.append(divider) : null;
 
